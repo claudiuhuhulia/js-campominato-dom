54d31c88bab60abdfcf163ea18c7306787f26af0
cloudhuh <claudiu.huhulia@gmail.com>
2023-06-08 16:41:16 +0200

    Score section js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+
+/* DOM ELEMENTS */
+
 const grid = document.getElementById('grid');
 const button= document.getElementById('play');
+const scoreElement = document.getElementById('score');
 
+
+/* FUNCTIONS */
 
 const createCell = () => {
     const cell = document.createElement('div');
@@ -9,20 +15,30 @@ const createCell = () => {
 } 
 
 
+
+
 button.addEventListener('click', function(){
 
+  grid.innerText='';
+  let score = 0;
     for(i= 0 ; i < 100; i ++){
       const cell = createCell();  
       
       const cellnumb = cell.innerText= i + 1;
-
+      
       cell.addEventListener('click', function(){
-
+        
         cell.classList.add('clicked');
-
-        console.log(cellnumb);
+        
+          ++score;
+          console.log(cellnumb);
+        
+        
+        
+        scoreElement.innerText= score;
       })
+      
+      
       grid.appendChild(cell);
     }
-
 })
